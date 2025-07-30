ca8ad89ef10022db02e6208faf315ab7b0264fdc
Soham Kalghatgi <skalghatgi@dspace.de>
2025-07-30 12:51:54 +0200

    Resource names updated

--- modules/scenario_generation_instance/backup.tf
+++ modules/scenario_generation_instance/backup.tf
@@ -23,8 +23,8 @@ resource "aws_backup_plan" "scenario_generation" {
 resource "aws_backup_selection" "scenario_generation_rds" {
   count        = var.enable_backup_service ? 1 : 0
   name         = "${local.instancename}-rds"
-  iam_role_arn = aws_iam_role.scenario_generation_backup_iam_role[0].arn
-  plan_id      = aws_backup_plan.aws_backup_plan[0].id
+  iam_role_arn = aws_iam_role.scenario_generation_backup_role[0].arn
+  plan_id      = aws_backup_plan.scenario_generation[0].id
   resources    = local.backup_resources
 }
 
