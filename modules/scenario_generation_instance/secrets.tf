10389b6ff6c70f050d1b53f8fbecb7eb1963c2d4
Soham Kalghatgi <skalghatgi@dspace.de>
2025-08-12 12:46:13 +0200

    OpenSearch secretes removed

--- modules/scenario_generation_instance/secrets.tf
+++ modules/scenario_generation_instance/secrets.tf
@@ -1,12 +1,3 @@
-data "aws_secretsmanager_secret" "opensearch_secret" {
-  count = var.opensearch.enable ? 1 : 0
-  name  = var.opensearch.master_user_secret_name
-}
-data "aws_secretsmanager_secret_version" "opensearch_secret" {
-  count     = var.opensearch.enable ? 1 : 0
-  secret_id = data.aws_secretsmanager_secret.opensearch_secret[0].id
-}
-
 data "aws_secretsmanager_secret" "postgresql_secret" {
   name = var.secretname
 }
