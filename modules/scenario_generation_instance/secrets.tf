cbb4e342b41697032ff813d9091e26573612a81e
Soham Kalghatgi <skalghatgi@dspace.de>
2025-08-13 10:49:22 +0200

    master user for open search removed from variables.tf

--- modules/scenario_generation_instance/secrets.tf
+++ modules/scenario_generation_instance/secrets.tf
@@ -1,6 +1,7 @@
 data "aws_secretsmanager_secret" "postgresql_secret" {
   name = var.secretname
 }
+
 data "aws_secretsmanager_secret_version" "postgresql_secret" {
   secret_id = data.aws_secretsmanager_secret.postgresql_secret.id
 }
