a681dac4a293e3cb0531b51870f0e5bb4f4021d4
Christian Bergen <cbergen@dspace.de>
2025-11-03 12:15:54 +0100

    check AL2

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -105,8 +105,9 @@ variable "node_groups" {
     max_size        = number
     min_size        = number
     custom_ami_id   = optional(string, "")
-    ami_type        = optional(string, "AL2023_x86_64_STANDARD")
-    #ami_type          = optional(string, "BOTTLEROCKET_x86_64")
+    ami_type        = optional(string, "AL2_x86_64")
+    #ami_type        = optional(string, "AL2023_x86_64_STANDARD")
+    #ami_type         = optional(string, "BOTTLEROCKET_x86_64")
     block_device_name = optional(string, "/dev/xvda")
     volume_size       = number
     k8s_labels        = optional(map(string), {})
