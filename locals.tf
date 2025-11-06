e3ebe94be98c42d944499b8669ba4ac940f15bdf
Christian Bergen <cbergen@dspace.de>
2025-11-06 09:41:36 +0100

    again jammy

--- locals.tf
+++ locals.tf
@@ -1,18 +1,12 @@
-# data "aws_ami" "gpu_ami" {
-#   owners      = ["amazon"]
-#   most_recent = true
-#   filter {
-#     name   = "name"
-#     values = ["*ubuntu-eks/k8s_${var.kubernetesVersion}/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server*"]
-#     # from kubernetes version 1.33 use *ubuntu-eks/k8s_${var.kubernetesVersion}/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server*
-#   }
-# }
-
-
-data "aws_ssm_parameter" "bottlerocket_nvidia_ami" {
-  name = "/aws/service/bottlerocket/aws-k8s-${var.kubernetesVersion}-nvidia/x86_64/latest/image_id"
+data "aws_ami" "gpu_ami" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["*ubuntu-eks/k8s_${var.kubernetesVersion}/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server*"]
+    # from kubernetes version 1.33 use *ubuntu-eks/k8s_${var.kubernetesVersion}/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server*
+  }
 }
-
 locals {
   create_vpc                                = var.vpcId == null ? true : false
   vpc_id                                    = local.create_vpc ? module.vpc[0].vpc_id : var.vpcId
@@ -108,8 +102,8 @@ locals {
       subnet_ids        = local.private_subnets
       max_size          = var.gpuNodeCountMax
       min_size          = var.gpuNodeCountMin
-      custom_ami_id     = data.aws_ssm_parameter.bottlerocket_nvidia_ami.value
-      block_device_name = "/dev/xvdb"
+      custom_ami_id     = data.aws_ami.gpu_ami.image_id
+      block_device_name = "/dev/sda1"
       volume_size       = var.gpuNodeDiskSize
       k8s_labels = {
         "purpose"    = "gpu",
@@ -131,7 +125,7 @@ locals {
       subnet_ids        = local.private_subnets
       max_size          = var.ivsGpuNodeCountMax
       min_size          = var.ivsGpuNodeCountMin
-      custom_ami_id     = data.aws_ssm_parameter.bottlerocket_nvidia_ami.value
+      custom_ami_id     = data.aws_ami.gpu_ami.image_id
       block_device_name = "/dev/sda1"
       volume_size       = var.ivsGpuNodeDiskSize
       k8s_labels = {
