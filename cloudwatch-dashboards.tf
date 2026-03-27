a3c53177ff87538bc41f50cd057a615f22716a0c
Mario Gluhakovic <mgluhakovic@dspace.hr>
2026-03-27 13:38:42 +0100

    Format terraform files

--- cloudwatch-dashboards.tf
+++ cloudwatch-dashboards.tf
@@ -7,10 +7,10 @@ locals {
 }
 
 resource "aws_cloudwatch_dashboard" "cloudwatch_dashboard" {
-    for_each       = toset(local.dashboards)
-    dashboard_name = each.value
-    dashboard_body = templatefile("${path.module}/templates/cloudwatch_dashboards/${each.value}.json", {
-        region = local.region,
-        cluster_name = var.infrastructurename
-    })
+  for_each       = toset(local.dashboards)
+  dashboard_name = each.value
+  dashboard_body = templatefile("${path.module}/templates/cloudwatch_dashboards/${each.value}.json", {
+    region       = local.region,
+    cluster_name = var.infrastructurename
+  })
 }
