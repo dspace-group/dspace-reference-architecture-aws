67df0f7188e3e1c7fae87269c4c9b064df86e3d5
lukabudak <161831514+lukabudak@users.noreply.github.com>
2026-04-30 12:31:51 +0200

    fix: change deprecated tls_security_policy option (#268)

--- modules/ivs_aws_instance/opensearch.tf
+++ modules/ivs_aws_instance/opensearch.tf
@@ -18,7 +18,7 @@ resource "aws_opensearch_domain" "opensearch" {
   }
   domain_endpoint_options {
     enforce_https       = true
-    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
+    tls_security_policy = "Policy-Min-TLS-1-2-PFS-2023-10"
   }
   cluster_config {
     instance_count         = var.opensearch.instance_count
