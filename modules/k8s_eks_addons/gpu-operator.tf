6506aac9ee9abdc0e731a55d55ca6dea0a07b544
Mario Gluhakovic <mgluhakovic@dspace.hr>
2026-08-10 15:25:13 +0200

    Fix formating

--- modules/k8s_eks_addons/gpu-operator.tf
+++ modules/k8s_eks_addons/gpu-operator.tf
@@ -30,7 +30,7 @@ resource "kubectl_manifest" "nvidia-driver-manifest" {
 
   manifest = {
     apiVersion = "nvidia.com/v1alpha1"
-    kind = "NVIDIADriver"
+    kind       = "NVIDIADriver"
 
     metadata = {
       name = "driver-gpu-nodes-${each.value}"
@@ -38,25 +38,25 @@ resource "kubectl_manifest" "nvidia-driver-manifest" {
 
     spec = {
       driverType = "gpu"
-      image = "driver"
+      image      = "driver"
       repository = "nvcr.io/nvidia"
-      version = "${each.key}"
+      version    = "${each.key}"
       nodeSelector = {
         gpu-driver = "${each.key}"
       }
 
       tolerations = [
         {
-          key = "purpose"
+          key      = "purpose"
           operator = "Equal"
-          value = "gpu"
-          effect = "NoSchedule"
+          value    = "gpu"
+          effect   = "NoSchedule"
         },
         {
-          key = "nvidia.com/gpu"
-          value = ""
+          key      = "nvidia.com/gpu"
+          value    = ""
           operator = "Exists"
-          effect = "NoSchedule"
+          effect   = "NoSchedule"
         }
       ]
     }
